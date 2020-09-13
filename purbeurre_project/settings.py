3dc96731dd2034419ae2ef9153cd1919b787fd1f
raoofrachidi <43079354+raoofrachidi@users.noreply.github.com>
2020-09-14 01:13:38 +0200

    Update settings.py

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -136,11 +136,11 @@ USE_TZ = True
 INTERNAL_IPS = ['127.0.0.1']
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 if os.environ.get('ENV') == 'PRODUCTION':
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     STATICFILES_DIRS = os.path.join(PROJECT_ROOT, 'static')
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
